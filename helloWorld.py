3c9152c31e2b457ffc89b4522082d58aaf2b2974
RAL95 <81445304+RAL95@users.noreply.github.com>
2021-03-26 22:03:43 -0400

    (added face user and password)

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,9 @@
 print ("Hello World, my name is Romer and I love Python!")
-userName = input ("Hello! Please enter your name!")
+userName = input ("Hello! Please enter your facebook username!")
+userPassword = input("Hello! Please enter your facebook password!")
 print ("Hello", userName, "nice to meet you")
+if(userName.lower() == "romer" and userPassword == "ihopethisworks!"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
 
